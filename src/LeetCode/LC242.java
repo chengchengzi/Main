3e71b2ddce0dbad56df47d55657f93b7acf77b04
chengchengzi <sueannika@163.com>
2020-04-27 23:49:21 +0800

    leetcode LC242

---
+++ src/LeetCode/LC242.java
@@ -0,0 +1,74 @@
+package LeetCode;
+
+import javax.xml.stream.events.Characters;
+import java.util.Arrays;
+
+public class LC242{
+    /**
+     *    242.有效的字母异位词
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     *
+     * 示例 1:
+     *
+     * 输入: s = "anagram", t = "nagaram"
+     * 输出: true
+     * 示例 2:
+     *
+     * 输入: s = "rat", t = "car"
+     * 输出: false
+     * 说明:
+     * 你可以假设字符串只包含小写字母。
+     *。
+     */
+
+    /*
+        解法1
+        时间复杂度：O(nlogn)
+        空间复杂度：O(s.length() + t.length())
+        1. 分别将 s 和 t排序，排序都相等则为字母异位词，否则不是
+     */
+    public boolean isAnagram(String s, String t) {
+        if(s.length() == 0 && t.length() == 0) {
+            return true;
+        }
+        if(s.length() == 0 || t.length() == 0){
+            return false;
+        }
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] sTmp = s.toCharArray();
+        char[] tTmp = t.toCharArray();
+        Arrays.sort(sTmp);
+        Arrays.sort(tTmp);
+        return Arrays.equals(sTmp,tTmp);
+    }
+
+    /*
+        解法2：O(N), O(1)
+        1、在a~z中，分别统计s和t中字母的数量，存放在一个int数组中，都相等则为true，不等的则为false
+     */
+    public boolean isAnagram2(String s, String t) {
+        if(s.length() == 0 && t.length() == 0) {
+            return true;
+        }
+        if(s.length() == 0 || t.length() == 0){
+            return false;
+        }
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] cnt = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            cnt[s.charAt(i) - 'a']++;
+            cnt[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < cnt.length; i++){
+            if(cnt[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import JZOffer.JZ59I;
+import LeetCode.LC242;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,3,-1,-3,5,3,6,7};
-        int k = 3;
-        int[] res = new JZ59I().maxSlidingWindow2(nums,k);
-        for(int i = 0; i < res.length; i++){
-            System.out.print(res[i]);
-        }
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(new LC242().isAnagram2(s,t));
     }
 }
