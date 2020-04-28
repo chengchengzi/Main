0510029db91f6e453c6a6e34d936ac25fdfb1219
chengchengzi <sueannika@163.com>
2020-04-28 22:55:20 +0800

    jzoffer 59-I题

--- src/JZOffer/JZ55I.java
+++ src/JZOffer/JZ55I.java
@@ -1,5 +1,8 @@
 package JZOffer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class JZ55I {
     /**
      * 面试题55 - I. 二叉树的深度
@@ -30,5 +33,35 @@ public class JZ55I {
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         return left > right ? left + 1: right + 1;
+    }
+
+    /*
+        层序遍历
+     */
+    public int maxDepth2(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        TreeNode currentEnd = root; //记录当前层的最右结点
+        TreeNode nextEnd = root; //记录下一层的最右结点
+        while(!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            if(current.left != null){
+                queue.add(current.left);
+                nextEnd = current.left;
+            }
+            if(current.right != null){
+                queue.add(current.right);
+                nextEnd = current.right;
+            }
+            if(current == currentEnd) {  //判断当前节点是否到达该层的最右节点，是则更新最右结点
+                depth++;
+                currentEnd = nextEnd;
+            }
+        }
+        return depth;
     }
 }
