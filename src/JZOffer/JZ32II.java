e6b11c72497d2b59a7bbd647efc2fd126a910e4a
chengchengzi <sueannika@163.com>
2020-05-01 22:31:03 +0800

    jzoffer 32-II题

--- src/JZOffer/JZ32II.java
+++ src/JZOffer/JZ32II.java
@@ -32,6 +32,7 @@ public class JZ32II {
         借助队列
      */
      public List<List<Integer>> levelOrder(TreeNode root) {
+         //利用currentEnd和nextNode结点来标记是否到达层结尾结点
          List<List<Integer>> list = new ArrayList<>();
          if(root == null){
              return list;
@@ -59,5 +60,33 @@ public class JZ32II {
              }
          }
          return list;
+         /*
+         //另一种写法：利用队列的size()
+         List<List<Integer>> list = new ArrayList<>();
+         if(root == null){
+             return list;
+         }
+         Queue<TreeNode> queue = new LinkedList<>();
+         queue.add(root);
+         while(!queue.isEmpty()){
+             int len = queue.size();
+             int i = 0;
+             ArrayList<Integer> tmp2 = new ArrayList<>();
+             while(i < len && !queue.isEmpty()){
+                TreeNode p = queue.poll();
+                tmp2.add(p.val);
+                if(p.left != null){
+                     queue.add(p.left);
+                }
+                if(p.right != null){
+                     queue.add(p.right);
+                }
+                i++;
+             }
+             list.add(tmp2);
+         }
+         return list;
+     }
+          */
      }
 }
