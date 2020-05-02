a474d940b7ecd353eb871d7ddb699501d2e862c9
chengchengzi <sueannika@163.com>
2020-05-02 23:16:25 +0800

    jzoffer 59-I题

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import JZOffer.JZ59I;
 import LeetCode.LC242;
 
+import java.util.Arrays;
+
 public class Main {
     public static int cntBits(int num){
         int res = 0;
@@ -16,7 +18,7 @@ public class Main {
         for (int i = 0; i < nums.length; i++) {
             numsSorted[i] = nums[i];
         }
-        Arrays.sort(numsSorted, (num1,num2) -> cntBits(num1) == cntBits(num2) ? num1 - num2 : cntBits(num1) - cntBits(num2));
+        Arrays.sort(numsSorted, (num1, num2) -> cntBits(num1) == cntBits(num2) ? num1 - num2 : cntBits(num1) - cntBits(num2));
         int[] res = new int[numsSorted.length];
         int index = 0;
         for(Integer num : numsSorted){
@@ -28,10 +30,7 @@ public class Main {
     public static void main(String[] args) {
         int [] nums1 = {1,2,2,1};
         int[] nums2 = {2,2};
-        int[] res = new LC350().intersect2(nums1,nums2);
-        for(int i : res){
-            System.out.println(i);
-        }
+
     }
 }
 
