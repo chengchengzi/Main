a1182469ba7b22ea67b2a320d16046e742f011a4
chengchengzi <sueannika@163.com>
2020-05-02 21:51:18 +0800

    LC350

--- src/LeetCode/LC350.java
+++ src/LeetCode/LC350.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class LC350 {
     /**
@@ -44,6 +45,37 @@ public class LC350 {
                 a++;
             }else{
                 b++;
+            }
+        }
+        int[] res = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    /*
+        解法2：O(N)/O(M),O(M+N)
+        1、使用HashMap，key-value: nums1[i],次数
+        2、统计key-value相同的
+     */
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return new int[0];
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums1.length; i++){
+            if(map.containsKey(nums1[i])){
+                map.put(nums1[i],map.get(nums1[i]) + 1);
+            }else{
+                map.put(nums1[i],1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums2.length; i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0){
+                list.add(nums2[i]);
+                map.put(nums2[i],map.get(nums2[i]) - 1);
             }
         }
         int[] res = new int[list.size()];
