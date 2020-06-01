61f0855c361fcb6218bab8401f2ad217273c6e23
chengchengzi <sueannika@163.com>
2020-06-01 21:27:16 +0800

    jzoffer 11

---
+++ src/jzoffer/JZ11.java
@@ -0,0 +1,68 @@
+package jzoffer;
+
+import java.util.Arrays;
+
+public class JZ11 {
+    /**
+     * 面试题11. 旋转数组的最小数字
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+     *
+     * 示例 1：
+     *
+     * 输入：[3,4,5,1,2]
+     * 输出：1
+     * 示例 2：
+     *
+     * 输入：[2,2,2,0,1]
+     * 输出：0
+     */
+
+    /*
+        解法1：O(NlogN),O(1)
+        思路：排序，直接返回第一个
+     */
+    public int minArray(int[] numbers) {
+        if(numbers.length == 0)
+            return -1;
+        Arrays.sort(numbers);
+        return numbers[0];
+    }
+
+    /*
+        解法2：O(N),O(1)
+        思路：直接查找最小值
+     */
+    public int minArray2(int[] numbers) {
+        if(numbers.length == 0)
+            return -1;
+        int min = numbers[0];
+        for(int i = 1; i < numbers.length; i++){
+            if(numbers[i] <= min){
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+    /*
+        解法3：O（logN）,O(1)
+        思路：二分查找
+     */
+    public int minArray3(int[] numbers) {
+        if (numbers.length == 0)
+            return -1;
+        int left = 0;
+        int right = numbers.length - 1;
+        while(left != right){
+            int mid = left + (right - left) / 2;
+            if(numbers[mid] < numbers[right]){
+                right = mid;
+            }else if(numbers[mid] > numbers[right]){
+                left = mid + 1;
+            }else {
+                right--;
+            }
+        }
+        return numbers[left];
+    }
+}
